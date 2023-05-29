35e6ea56b221cb94cf2db2b5c2d7e151273d642f
fred <domaine75@gmail.com>
2023-05-29 10:55:11 +0200

    Clean code and fix SQL request to NorthWind database

--- DataDashboardApp/Db/DbConnection.cs
+++ DataDashboardApp/Db/DbConnection.cs
@@ -1,19 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data.SqlClient;
 
 namespace DashboardApp.Db
 {
-    public abstract class DbConnection
+  public abstract class DbConnection
     {
         private readonly string connectionString;
 
         public DbConnection()
         {
-            connectionString = "Server=(local); DataBase=NorthwindStore; Integrated Security=true";
+            connectionString = "Server=localhost; DataBase=Northwind; Integrated Security=true";
         }
 
         protected SqlConnection GetConnection()
